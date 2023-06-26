b9a7665e8be5cc1784db9e113589f39e292f2eb2
kezdetphia <fehermark88@gmail.com>
2023-06-26 23:28:09 +0200

    wrote logic in the index.js and set up index.ejs

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,33 @@ const fetch = require('node-fetch')
 
 const token = process.env.GITHUB_TOKEN
 
+const ROOT_URL = 'https://api.github.com';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next)=> {
+  const username = req.query.username
+
+  if(!username) return res.render('index', {userData:null})
+
+  const options = {
+    headers: {
+      Authorizaton : `token${token}`,
+    }
+  }
+  try{
+    const userData = await fetch(`$${ROOT_URL}/users/${username}`, options)
+    .then(res=>res.json())
+    res.render('index', { title: 'Express' });
+  }catch(err){
+    console.log(err)
+  }
 });
+
+
+
+
+
+
+
 
 module.exports = router;
