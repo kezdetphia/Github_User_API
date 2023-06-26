dceb28188a71ecad35a5fdb0b710ee290954349d
kezdetphia <fehermark88@gmail.com>
2023-06-26 23:21:13 +0200

    installed dependencies

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch')
+
+
+const token = process.env.GITHUB_TOKEN
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
